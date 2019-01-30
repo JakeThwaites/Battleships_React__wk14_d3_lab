6a3483a3e3eff70b40f01704d77d7bf090ad2700
Jake Thwaites <jakethwaites@hotmail.co.uk>
2019-01-30 15:33:25 +0000

    amended css for win state

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     const ship = new Ship([1,2,3]);
     const ship2 = new Ship([8,9]);
-    const ship3 = new Ship([13]);
+    const ship3 = new Ship([69,79,89, 99]);
     const arrayOfShips = [ship, ship2, ship3];
     const player = new Player(arrayOfShips);
     const game = new Game(player);
